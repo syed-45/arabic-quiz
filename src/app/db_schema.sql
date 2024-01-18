1e17a35df5120f24b9505066d868592629b4704c
Syed Haque <haque_45@hotmail.co.uk>
2024-01-18 20:08:09 +0000

    initial_plan+initial_db_schema

---
+++ src/app/db_schema.sql
@@ -0,0 +1,67 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    -- The 'VARCHAR' data type is used to store variable-length character strings.
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+CREATE table chapters (
+    chapter_number INT PRIMARY KEY,
+    chapter_name VARCHAR(255),
+    chapter_arabic_name VARCHAR(255),
+);
+
+CREATE TABLE chapter_one_verbs (
+    id SERIAL PRIMARY KEY,
+    arabic VARCHAR(255),
+    english VARCHAR(255)
+);
+
+INSERT INTO chapter_one_verbs (arabic, english) VALUES
+    ('نَظَر', 'To look'),
+    ('اِسْتَمَع', 'To listen'),
+    ('عَاد', 'To repeat'),
+    ('أَشَارَ', 'To point, indicate'),
+    ('وَضَع', 'To put'),
+    ('سَمِع', 'To hear'),
+    ('قَال', 'To say'),
+    ('رَتَّب', 'To put in order, organise, arrange'),
+    ('تَبَادَل', 'To exchange'),
+    ('سَأَل', 'To ask'),
+    ('أَجَاب', 'To answer'),
+    ('قَرَأ', 'To read'),
+    ('مَرَّ', 'To pass by'),
+    ('نَسَخ', 'To copy');
+
+CREATE TABLE chapter_one_nouns (
+    id SERIAL PRIMARY KEY,
+    arabic VARCHAR(255),
+    english VARCHAR(255),
+    arabic_plural VARCHAR(255)
+);
+
+INSERT INTO chapter_one_nouns (arabic, english, arabic_plural) VALUES
+    ('طَالِب', 'Student', 'طُلَّاب'),
+    ('كِتَاب', 'Book', 'كُتُب'),
+    ('وَحْدَة', 'Chapter', 'وَحَدَات'),
+    ('لُغَة', 'Language', 'لُغَات'),
+    ('جِنْسِيَّة', 'Nationality', 'جِنْسِيَّات'),
+    ('اِسْم', 'Name', 'أَسْمَاء'),
+    ('أَخ', 'Brother', 'إِخْوَة'),
+    ('مُهَنْدِس', 'Engineer', 'مُهَنْدِسُون'),
+    ('صَدِيق', 'Friend', 'أَصْدِقَاء'),
+    ('طَبِيب', 'Doctor', 'أَطْبَاء'),
+    ('أُخْت', 'Sister', 'أَخَوَات'),
+    ('دَرْس', 'Lesson', 'دُرُوس'),
+    ('تَدْرِيب', 'Exercise', 'تَدَرِيبَات'),
+    ('عَدَد', 'Number', 'أَعْدَاد'),
+    ('صُورَة', 'Picture', 'صُوَر'),
+    ('جَوَاب', 'Answer', 'أَجْوِبَة'),
+    ('سُؤَال', 'Question', 'أَسْئِلَة'),
+    ('مِثَال', 'Example', 'أَمْثِلَة'),
+    ('زَمِيل', 'Colleague, classmate', 'زُمَلَاء'),
+    ('رِقْم', 'Number', 'أَرْقَام'),
+    ('جُمْلَة', 'Sentence', 'جُمَل'),
+    ('عَلَامَة', 'Sign, indication', 'عَلَامَات'),
+    ('كَلِمَة', 'Word', 'كَلِمَات');
