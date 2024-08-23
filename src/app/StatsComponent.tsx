3fc4734452c0d302514f557687b25e58a07cd6d5
Syed Haque <haque_45@hotmail.co.uk>
2024-08-23 15:34:59 +0100

    align stats w /16 and % symbols

--- src/app/StatsComponent.tsx
+++ src/app/StatsComponent.tsx
@@ -31,13 +31,13 @@ export const StatsComponent = ({quizzesCompleted, percentageScore}: StatsCompone
     return (
         <div className="grid grid-cols-2 pt-5 px-5 text-7xl sm:text-9xl sm:py-7 max-w-screen-sm mx-auto gap-x-5 gap-y-2">
             <div className="flex items-end justify-center">
-                <span className={`bg-gradient-to-r ${quizzesCompletedDisplay===quizzesCompleted ? "animate-moving-gradient-bg bg-[length:400%_400%] from-sky-400  via-sky-700 dark:via-sky-200 dark:to-sky-400 to-sky-400" : "from-sky-200 to-sky-500"} bg-clip-text drop-shadow-md text-transparent`}>
+                <span className={`bg-gradient-to-r ${quizzesCompletedDisplay===quizzesCompleted ? "animate-moving-gradient-bg bg-[length:400%_400%] from-sky-400  via-sky-700 dark:via-sky-200 dark:to-sky-400 to-sky-400" : "from-sky-200 to-sky-500"} bg-clip-text drop-shadow-md text-transparent leading-[0.83] sm:leading-[0.79]`}>
                      {quizzesCompletedDisplay} 
                 </span>
                 {quizzesCompletedDisplay===quizzesCompleted && <span className="text-xs animate-fade-out">/ 16</span>}
             </div>
             <div className="flex items-end justify-center">
-                <span className={`bg-gradient-to-r ${percentageScoreDisplay===percentageScore ? "animate-moving-gradient-bg bg-[length:400%_400%] from-green-400 via-green-600 dark:via-green-200 dark:to-green-400 to-green-400" : "from-green-200 to-green-600"} bg-clip-text drop-shadow-md text-transparent`}>
+                <span className={`bg-gradient-to-r ${percentageScoreDisplay===percentageScore ? "animate-moving-gradient-bg bg-[length:400%_400%] from-green-400 via-green-600 dark:via-green-200 dark:to-green-400 to-green-400" : "from-green-200 to-green-600"} bg-clip-text drop-shadow-md text-transparent leading-[0.83] sm:leading-[0.79]`}>
                     {percentageScoreDisplay} 
                 </span>
                 {percentageScoreDisplay===percentageScore && <span className="text-xs animate-fade-out">%</span>}
