8221a147ed4bf637b276b2f1c1523e5738acc675
Syed Haque <haque_45@hotmail.co.uk>
2025-07-16 15:51:56 +0100

    fix production error with manifest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Arabic Quiz App',
+    short_name: 'ArabicQuizApp',
+    description: 'Next.js App',
+    start_url: '/',
+    icons: [
+    {
+      src: '/web-app-manifest-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'maskable'
+    },
+    {
+      src: '/web-app-manifest-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable'
+    }
+  ],
+  "theme_color": "#ffffff",
+  "background_color": "#ffffff",
+  "display": "standalone"
+  }
+}
